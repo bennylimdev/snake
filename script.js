52fb003815deadbf18a624a3127eda80dc3e965b
Ben Lim <finaldessert@gmail.com>
2023-12-14 16:17:47 -0500

    Add control functionality with Eventlistener

--- script.js
+++ script.js
@@ -11,12 +11,34 @@ function generateFood() {
 }
 
 function renderGame() {
-    let updatedGame = `<div class="food" style="grid-area: ${foodX}/${foodY};"></div>`;
+    let updatedGame = `<div class="food" style="grid-area: ${foodY}/${foodX};"></div>`;
+
+    headX += velocityX;
+    headY += velocityY;
     
-    updatedGame += `<div class="snake" style="grid-area: ${headX}/${headY};"></div>`;
+    updatedGame += `<div class="snake" style="grid-area: ${headY}/${headX};"></div>`;
 
     gameContainer.innerHTML = updatedGame;
 }
 
 generateFood();
-renderGame();
+setInterval(renderGame, 150);
+
+// if this happens run this function //
+document.addEventListener("keydown", function(e){
+    console.log(e.key);
+    let key = e.key;
+    if(key == "w") {
+        velocityX = 0;
+        velocityY = -1;
+    } else if(key == "s") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if(key == "a") {
+        velocityY = 0;
+        velocityX = -1;
+    } else if(key =="d") {
+        velocityY = 0;
+        velocityX = 1;
+    }
+})
