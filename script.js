6c53659f1d0525e04050ab6565f774a5faed67f7
Ben Lim <finaldessert@gmail.com>
2023-12-14 15:44:18 -0500

    Update intructions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let gameContainer = document.querySelector(".game-container")
 
-let foodX;
-let foodY;
+let foodX, foodY;
+let headX = 12, headY = 12;
+let velocityX = 0, velocityY = 0;
 
 function generateFood() {
     foodX = Math.floor(Math.random()*25) + 1;
     foodY = Math.floor(Math.random()*25) + 1;
 
 }
+
 function renderGame() {
-    let updatedGame = `<div class="food" style="grid-area: ${foodY}/${foodX};"></div>`;
+    let updatedGame = `<div class="food" style="grid-area: ${foodX}/${foodY};"></div>`;
+    
+    updatedGame += `<div class="snake" style="grid-area: ${headX}/${headY};"></div>`;
 
     gameContainer.innerHTML = updatedGame;
 }
